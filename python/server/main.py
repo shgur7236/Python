32c2bbb49ae60a95c7b3f7c5dbec500f7f24ad27
Noh-hyuck <81364498+shgur7236@users.noreply.github.com>
2021-07-14 10:00:29 +0900

    Add files via upload

--- python/server/main.py
+++ python/server/main.py
@@ -1,14 +1,19 @@
+#POST: 데이터를 생성하기 위해.
+#GET: 데이터를 읽기 위해.
+#PUT: 데이터를 업데이트하기 위해.
+#DELETE: 데이터를 삭제하기 위해.
+
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {"Hello":{"World":"HI"}}
+@app.get('/users/me')
+async def read_user_me():
+    return {"user_id":"the current user"}
 
-@app.get('/about') 
-def about():
-    return {'deta':'about page'}
- 
+@app.get('/users/{user_id}')
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+
